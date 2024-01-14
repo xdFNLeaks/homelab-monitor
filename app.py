c3b502ffec44f059f9607eff189d7d7b8dfd7238
xdFNLeaks <72074685+xdFNLeaks@users.noreply.github.com>
2024-01-15 01:29:41 +1100

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def home():
     return render_template('index.html', cpu_percent=cpu_percent, memory_info=memory_info)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
