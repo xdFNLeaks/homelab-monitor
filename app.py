888a6875fd0c8e737d36b8cfb91d666178b1327e
xdFNLeaks <72074685+xdFNLeaks@users.noreply.github.com>
2024-01-15 01:06:09 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import psutil
 
 app = Flask(__name__)
 
-@app.route('/')                                                   def home():
-    cpu_percent = psutil.cpu_percent()                                memory_info = psutil.virtual_memory()
-                                                                      return render_template('index.html', cpu_percent=cpu_percent, memory_info=memory_info)
-                                                                  if __name__ == '__main__':
+@app.route('/')
+def home():
+    cpu_percent = psutil.cpu_percent()
+    memory_info = psutil.virtual_memory()
+    return render_template('index.html', cpu_percent=cpu_percent, memory_info=memory_info)
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
